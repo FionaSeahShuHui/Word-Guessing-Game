8a8a87fe10558ca8331919192882fd4993b65568
FionaSeahShuHui <fionaseah98@gmail.com>
2023-03-05 20:48:17 +0800

    Updated word list V1

--- words.js
+++ words.js
@@ -1,198 +1,58 @@
 const wordList = [
   {
-    word: "python",
-    hint: "programming language",
+    word: "baby",
+    hint: "A young child",
   },
   {
-    word: "guitar",
-    hint: "a musical instrument",
+    word: "bee",
+    hint: "Winged insects closely related to wasps and ants",
   },
   {
-    word: "aim",
-    hint: "a purpose or intention",
+    word: "bowl",
+    hint: "A round container with wide uncovered top",
   },
   {
-    word: "venus",
-    hint: "planet of our solar system",
+    word: "caterpilla",
+    hint: "The larva of a butterfly or moth",
   },
   {
-    word: "gold",
-    hint: "a yellow precious metal",
+    word: "camera",
+    hint: "A device for recording an image",
   },
   {
-    word: "ebay",
-    hint: "online shopping site",
+    word: "cloud",
+    hint: "A term used to describe a global network of servers, each with unique function",
   },
   {
-    word: "golang",
-    hint: "programming language",
+    word: "cube",
+    hint: "A solid shape with 6 square faces",
   },
   {
-    word: "coding",
-    hint: "related to programming",
+    word: "diamond",
+    hint: "A mineral composed of pure carbon",
   },
   {
-    word: "matrix",
-    hint: "science fiction movie",
+    word: "doll",
+    hint: "A small model of a human figure, used as a child's toy",
   },
   {
-    word: "bugs",
-    hint: "related to programming",
-  },
-  {
-    word: "avatar",
-    hint: "epic science fiction film",
-  },
-  {
-    word: "gif",
-    hint: "a file format for image",
-  },
-  {
-    word: "mental",
-    hint: "related to the mind",
-  },
-  {
-    word: "map",
-    hint: "diagram represent of an area",
-  },
-  {
-    word: "island",
-    hint: "land surrounded by water",
-  },
-  {
-    word: "hockey",
-    hint: "a famous outdoor game",
-  },
-  {
-    word: "chess",
-    hint: "related to an indoor game",
-  },
-  {
-    word: "viber",
-    hint: "a social media app",
-  },
-  {
-    word: "github",
-    hint: "code hosting platform",
-  },
-  {
-    word: "png",
-    hint: "a image file format",
-  },
-  {
-    word: "silver",
-    hint: "precious greyish-white metal",
-  },
-  {
-    word: "mobile",
-    hint: "an electronic device",
-  },
-  {
-    word: "gpu",
-    hint: "computer component",
-  },
-  {
-    word: "java",
-    hint: "programming language",
-  },
-  {
-    word: "google",
-    hint: "famous search engine",
-  },
-  {
-    word: "venice",
-    hint: "famous city of waters",
-  },
-  {
-    word: "excel",
-    hint: "microsoft product for windows",
-  },
-  {
-    word: "mysql",
-    hint: "a relational database system",
-  },
-  {
-    word: "nepal",
-    hint: "developing country name",
-  },
-  {
-    word: "flute",
-    hint: "a musical instrument",
-  },
-  {
-    word: "crypto",
-    hint: "related to cryptocurrency",
-  },
-  {
-    word: "tesla",
-    hint: "unit of magnetic flux density",
-  },
-  {
-    word: "mars",
-    hint: "planet of our solar system",
-  },
-  {
-    word: "proxy",
-    hint: "related to server application",
-  },
-  {
-    word: "email",
-    hint: "related to exchanging message",
-  },
-  {
-    word: "html",
-    hint: "markup language for the web",
-  },
-  {
-    word: "air",
-    hint: "related to a gas",
-  },
-  {
-    word: "idea",
-    hint: "a thought or suggestion",
-  },
-  {
-    word: "server",
-    hint: "related to computer or system",
-  },
-  {
-    word: "svg",
-    hint: "a vector image format",
-  },
-  {
-    word: "jpeg",
-    hint: "a image file format",
-  },
-  {
-    word: "search",
-    hint: "act to find something",
-  },
-  {
-    word: "key",
-    hint: "small piece of metal",
-  },
-  {
-    word: "egypt",
-    hint: "a country name",
-  },
-  {
-    word: "joker",
-    hint: "psychological thriller film",
-  },
-  {
-    word: "dubai",
-    hint: "developed country name",
-  },
-  {
-    word: "photo",
-    hint: "representation of person or scene",
-  },
-  {
-    word: "nile",
-    hint: "largest river in the world",
+    word: "duck",
+    hint: "A small type of swimming bird with short neck and legs",
   },
   {
     word: "rain",
-    hint: "related to a water",
+    hint: "The liquid form of water that falls from the sky",
+  },
+  {
+    word: "ribbon",
+    hint: "A knot tied with 2 loops and 2 loose ends, used especially for tying shoelaces",
+  },
+  {
+    word: "snake",
+    hint: "A long reptile without legs",
+  },
+  {
+    word: "stairs",
+    hint: "A structure designed to bridge a large vertical distance between lower and higher level",
   },
 ];
